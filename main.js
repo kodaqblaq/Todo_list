46f5f34111878d3f138c274e15319ae9008bf0c8
Kelvin Ntiri <68593771+kodaqblaq@users.noreply.github.com>
2020-08-04 11:12:44 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,3 @@ function add() {
     document.getElementsById('newdata') =
         document.getElementsByClassName('data').value;
 }
-document.write("i made everything up becouse i was aqussed of robbery")
