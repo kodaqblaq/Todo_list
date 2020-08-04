07935e5a6ad2a632bc411b5205b78544310bc864
Kelvin Ntiri <68593771+kodaqblaq@users.noreply.github.com>
2020-08-04 11:11:36 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function add() {
+    document.getElementsById('newdata') =
+        document.getElementsByClassName('data').value;
+}
+document.write("i made everything up becouse i was aqussed of robbery")
